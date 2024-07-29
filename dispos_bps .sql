807260ad9c7bd494467d664ba9c2648256e00ef6
laniisa <150539273+laniisa@users.noreply.github.com>
2024-07-29 08:08:33 +0700

    Update dispos_bps .sql

--- dispos_bps .sql
+++ dispos_bps .sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 26, 2024 at 04:08 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Generation Time: Jul 28, 2024 at 03:05 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,27 +24,32 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `berkas`
---
-
-CREATE TABLE `berkas` (
-  `id` int(11) NOT NULL,
-  `id_surat` int(11) NOT NULL,
-  `author` int(11) NOT NULL,
-  `berkas` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `kepala`
 --
 
 CREATE TABLE `kepala` (
-  `id` int(11) NOT NULL,
+  `id_ds_kepala` int(11) NOT NULL,
   `catatan_kepala` int(11) NOT NULL,
-  `tindak_lanjut` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `tindak_lanjut` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kepala`
+--
+
+INSERT INTO `kepala` (`id_ds_kepala`, `catatan_kepala`, `tindak_lanjut`, `id_user`) VALUES
+(1, 0, 0, 1);
+
+--
+-- Triggers `kepala`
+--
+DELIMITER $$
+CREATE TRIGGER `after_kepala_insert` AFTER INSERT ON `kepala` FOR EACH ROW BEGIN
+  INSERT INTO role (id_user, id_user_role) VALUES (NEW.id_user, 1);
+END
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -53,13 +58,26 @@ CREATE TABLE `kepala` (
 --
 
 CREATE TABLE `pegawai` (
-  `id` int(11) NOT NULL,
+  `id_ds_pegawai` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
   `id_surat` int(11) NOT NULL,
   `catatan` text NOT NULL,
   `tindak_lanjut` text NOT NULL,
-  `tanggal` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `tanggal` date NOT NULL,
+  `id_ds_kepala` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role`
+--
+
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_user_role` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -68,7 +86,7 @@ CREATE TABLE `pegawai` (
 --
 
 CREATE TABLE `surat` (
-  `id` int(11) NOT NULL,
+  `id_ds_surat` int(11) NOT NULL,
   `no_disposisi` varchar(20) NOT NULL,
   `no_surat` varchar(100) NOT NULL,
   `tgl_surat` date NOT NULL,
@@ -79,34 +97,19 @@ CREATE TABLE `surat` (
   `asal` varchar(100) NOT NULL,
   `jenis_surat` varchar(7) NOT NULL,
   `berkas` varchar(50) NOT NULL,
-  `status` char(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `status` char(1) NOT NULL,
+  `id_ds_kepala` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `surat`
 --
 
-INSERT INTO `surat` (`id`, `no_disposisi`, `no_surat`, `tgl_surat`, `tgl_input`, `tgl_disposisi`, `tgl_dilaksanakan`, `perihal`, `asal`, `jenis_surat`, `berkas`, `status`) VALUES
-(3, '76', '09', '2024-07-10', '2024-07-23', '2024-07-20', '2024-07-22', 'polimila', 'prov', 'resmi', '', 'd'),
-(4, '89', '738', '2024-07-02', '2024-07-24', '2024-07-13', '2024-07-13', 'mila', 'dncjc', 'nsjhd', 'nbkj', 'k'),
-(5, 'kk', '89', '2024-07-09', '2024-07-24', '2024-07-20', '2024-07-06', 'jis', 'nammna', 'ajh', 'ina', 'n'),
-(6, '88', '666', '2024-07-10', '2024-07-25', '2024-07-03', '2024-07-11', 'jn', 'huh', 'ii', 'uu', 'h'),
-(7, '11', '11', '2024-07-25', '2024-07-25', '2024-07-25', '2024-07-25', 'mad', 'ank', 'na', '1481-4640-1-PB.pdf', 'm'),
-(8, '12390', '33', '2024-07-25', '2024-07-25', '2024-07-25', '2024-07-25', 'amewe', 'a ,', 'ma', '770-Article_Text-2680-2-10-20220701_(1).pdf', 'm'),
-(9, '', '5', '2024-07-25', '2024-07-25', '0000-00-00', '0000-00-00', 'mla', 'mls', 'xa', '125998854.pdf', 'd');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
-
-CREATE TABLE `user` (
-  `email` varchar(50) NOT NULL,
-  `password` varchar(300) NOT NULL,
-  `id_role` int(11) DEFAULT NULL,
-  `is_active` int(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO `surat` (`id_ds_surat`, `no_disposisi`, `no_surat`, `tgl_surat`, `tgl_input`, `tgl_disposisi`, `tgl_dilaksanakan`, `perihal`, `asal`, `jenis_surat`, `berkas`, `status`, `id_ds_kepala`) VALUES
+(3, '76', '09', '2024-07-10', '2024-07-23', '2024-07-20', '2024-07-22', 'poli', 'prov', 'resmi', 'ko', 'd', 1),
+(4, '89', '738', '2024-07-02', '2024-07-24', '2024-07-13', '2024-07-13', 'mila', 'dncjc', 'nsjhd', 'nbkj', 'k', 1),
+(5, 'kk', '89', '2024-07-09', '2024-07-24', '2024-07-20', '2024-07-06', 'jis', 'nammna', 'ajh', 'ina', 'n', 1),
+(6, '88', '666', '2024-07-10', '2024-07-25', '2024-07-03', '2024-07-11', 'jn', 'huh', 'ii', 'uu', 'h', 1);
 
 -- --------------------------------------------------------
 
@@ -116,13 +119,13 @@ CREATE TABLE `user` (
 
 CREATE TABLE `users` (
   `id_user` int(11) NOT NULL,
-  `nama` varchar(50) NOT NULL,
-  `role` varchar(6) NOT NULL,
-  `status` varchar(7) NOT NULL,
-  `usr` varchar(20) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `whatsApp` varchar(15) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `nama` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role` varchar(6) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` varchar(7) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `usr` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `whatsApp` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -130,13 +133,11 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id_user`, `nama`, `role`, `status`, `usr`, `email`, `whatsApp`, `password`) VALUES
-(1, 'lani', '1', 'Active', 'laniisa', 'lan@gmail.com', '0876543', '$2y$10$PAN8FquBxZJ5tQjUN9r06uePvj9EFN.d6..WJd.kaUvoB2maJJqJK'),
+(1, 'laniio', 'Admin', 'Active', 'lanii', 'lan@gmail.com', '0876543', '$2y$10$MXkLH6Hadi67vU/rjmOh..r9K/Q2cd.i7jJnnhomjZPknQzh1Pcv.'),
 (2, 'Mila', '3', 'active', 'mila', 'mila@gmail.com', '0897654', '$2y$10$rcndayKmtvoTzxVvReqnFOq.HXEWVZEcv6q3BDLmvRK4lfhgdO3GW'),
 (3, 'laniiii', '2', 'active', 'sa', 'sa@gmail.com', '09876', 'sa'),
 (4, 'kami', '1', 'active', 'kami', 'kami@gmail.com', '08765', 'kami'),
-(6, 'Lani', 'Operat', 'Active', 'lllll', 'lll@gmail.com', '0765', '$2y$10$ITMBlP5woVeLknzNkRO5j.eCs/av2w12AYb8yGSLggT.evgY0WFoi'),
-(8, 'Lani', 'Operat', 'Active', 'lllll', 'lisaaa@gmail.com', '0765', '$2y$10$de2Ual3gJlQE99AUxwQE3eZ6z5Mbpa1LtNOaJsrFxxjz6t.CFXBFq'),
-(9, 'kamm', '3', 'active', 'kamm', 'kamm@gmail.com', '0785842', '$2y$10$acNJsFhnmUnRnAltjahoUeza7Hc44wuTU7KTstwfjCAU7kNBHrpYq');
+(7, 'k', 'Strukt', 'Active', 'kk', 'k@gmail.com', '00', '$2y$10$SI1Jylejbx7VVcdL8Smfgeg6fL3EHlLaUgwFUkh0h3c4u3h27wKPm');
 
 -- --------------------------------------------------------
 
@@ -145,15 +146,15 @@ INSERT INTO `users` (`id_user`, `nama`, `role`, `status`, `usr`, `email`, `whats
 --
 
 CREATE TABLE `user_role` (
-  `id` int(11) NOT NULL,
+  `id_user_role` int(11) NOT NULL,
   `role` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `user_role`
 --
 
-INSERT INTO `user_role` (`id`, `role`) VALUES
+INSERT INTO `user_role` (`id_user_role`, `role`) VALUES
 (0, 'admin'),
 (1, 'struktural'),
 (2, 'fungsional'),
@@ -164,34 +165,35 @@ INSERT INTO `user_role` (`id`, `role`) VALUES
 --
 
 --
--- Indexes for table `berkas`
---
-ALTER TABLE `berkas`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `kepala`
 --
 ALTER TABLE `kepala`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_ds_kepala`),
+  ADD KEY `fk_id_kepala` (`id_user`);
 
 --
 -- Indexes for table `pegawai`
 --
 ALTER TABLE `pegawai`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_ds_pegawai`),
+  ADD KEY `fk_kepala` (`id_ds_kepala`),
+  ADD KEY `fk_id_user` (`id_user`),
+  ADD KEY `fk_id_surat` (`id_surat`);
+
+--
+-- Indexes for table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_id_role_user` (`id_user`),
+  ADD KEY `fk_id_user_role` (`id_user_role`);
 
 --
 -- Indexes for table `surat`
 --
 ALTER TABLE `surat`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`email`);
+  ADD PRIMARY KEY (`id_ds_surat`),
+  ADD KEY `fk_id_ds_kepala` (`id_ds_kepala`);
 
 --
 -- Indexes for table `users`
@@ -204,41 +206,72 @@ ALTER TABLE `users`
 -- Indexes for table `user_role`
 --
 ALTER TABLE `user_role`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_user_role`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `berkas`
---
-ALTER TABLE `berkas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `kepala`
 --
 ALTER TABLE `kepala`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_ds_kepala` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `pegawai`
 --
 ALTER TABLE `pegawai`
+  MODIFY `id_ds_pegawai` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `role`
+--
+ALTER TABLE `role`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `surat`
 --
 ALTER TABLE `surat`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_ds_surat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `kepala`
+--
+ALTER TABLE `kepala`
+  ADD CONSTRAINT `fk_id_kepala` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`);
+
+--
+-- Constraints for table `pegawai`
+--
+ALTER TABLE `pegawai`
+  ADD CONSTRAINT `fk_id_surat` FOREIGN KEY (`id_surat`) REFERENCES `surat` (`id_ds_surat`),
+  ADD CONSTRAINT `fk_id_user` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`),
+  ADD CONSTRAINT `fk_kepala` FOREIGN KEY (`id_ds_kepala`) REFERENCES `kepala` (`id_ds_kepala`);
+
+--
+-- Constraints for table `role`
+--
+ALTER TABLE `role`
+  ADD CONSTRAINT `fk_id_role_user` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`),
+  ADD CONSTRAINT `fk_id_user_role` FOREIGN KEY (`id_user_role`) REFERENCES `user_role` (`id_user_role`);
+
+--
+-- Constraints for table `surat`
+--
+ALTER TABLE `surat`
+  ADD CONSTRAINT `fk_id_ds_kepala` FOREIGN KEY (`id_ds_kepala`) REFERENCES `kepala` (`id_ds_kepala`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
